1992242652613f64e2a0c2fa78a9edaa5701ccff
Dan-RAI <20709058+Dan-RAI@users.noreply.github.com>
2026-01-12 16:03:47 +0100

    inference func settings changed

--- xz-unsupervised--init.sql
+++ xz-unsupervised--init.sql
@@ -6,5 +6,5 @@ create type kmeans_grads_pluj as (gradients Float4[][],counts int[],stats Float4
 create or replace function kmeans(Text,Text,int,int,Float4,bool,int,bool) returns setof kmeansret as 'S|ai/sedn/unsupervised/Kmeans|kmeans_control_float_pluj|(Ljava/lang/String;Ljava/lang/String;IIFZIZ)Ljava/util/Iterator;' language ujava;
 create or replace function kmeans_gradients_tvm_float_pluj(Text,Text,int,Float4,int,Float4[]) returns  kmeans_grads_pluj as 'B|ai/sedn/unsupervised/Kmeans|kmeans_gradients_tvm_float_pluj|(Ljava/lang/String;Ljava/lang/String;IFI[F)Lai/sedn/unsupervised/GradientReturn;' LANGUAGE ujava;
 create or replace function kmeans_gradients_cpu_float_pluj(Text,Text,int,Float4,Float4[]) returns kmeans_grads_pluj as 'B|ai/sedn/unsupervised/Kmeans|kmeans_gradients_cpu_float_pluj|(Ljava/lang/String;Ljava/lang/String;IF[F)Lai/sedn/unsupervised/GradientReturn;' LANGUAGE ujava;
-create or replace function kmeans_inference_cpu_float_pluj(Float8[],Float4[]) returns int as 'F|ai/sedn/unsupervised/Kmeans|euclidean_distance_classmembership_cpu_float|([D[[F)I' LANGUAGE ujava;
+create or replace function kmeans_inference_cpu_float_pluj(Float8[],Float4[]) returns int as 'F|ai/sedn/unsupervised/Kmeans|euclidean_distance_classmembership_cpu_float|([D[[F)I' LANGUAGE ujava PARALLEL SAFE IMMUTABLE;
 
